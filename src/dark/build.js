d5162016de9edd3430bb2c1baf3544a37591ff3b
Alexander Senger <senger.alexander@yahoo.de>
2021-06-26 22:20:55 +0200

    setup dev command and excluded all but dark theme

--- src/dark/build.js
+++ src/dark/build.js
@@ -3,7 +3,8 @@ const mustache = require('mustache');
 
 const globals = require('./globals.json');
 const template = JSON.stringify(require(`${__dirname}/template.json`));
-const themeNames = ['horizon', 'horizon-italic', 'horizon-bold'];
+// const themeNames = ['horizon', 'horizon-italic', 'horizon-bold'];
+const themeNames = ['horizon'];
 
 for (themeName of themeNames) {
   const content = require(`${__dirname}/${themeName}.json`);
